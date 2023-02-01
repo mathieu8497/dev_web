3d9c01caaf16382dca4990356576ad6df7d86e91
Mathieu Poirel <mathieu8497@gmail.com>
2023-02-01 01:24:05 +0100

    Big changes to API, home and register

---
+++ assets/JavaScript/api.js
@@ -0,0 +1,28 @@
+    // Your OpenWeatherMap API key
+    const API_KEY = "631f9e3a166c7f20327ba73f59a26b08";
+  
+    // URL to get the weather data for Lyon
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=Lyon,FR&appid=${API_KEY}&units=metric`;
+  
+    // Fetch the weather data from OpenWeatherMap API
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        // Get the temperature in Lyon
+        const temperature = data.main.temp;
+  
+        // Display the temperature in the span tag
+        document.getElementById("temperature").innerText = temperature + "°C";
+      })
+      .catch(error => {
+        console.error(error);
+      });
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=Lyon&appid=${API_KEY}`)
+.then(response => response.json())
+.then(data => {
+  const iconId = data.weather[0].icon;
+  document.querySelector("#weather-icon").style.backgroundImage = `url(http://openweathermap.org/img/wn/${iconId}@2x.png)`;
+});
+
+      var postalAddress = document.getElementById("postalAddress");
+      var autocomplete = new google.maps.places.Autocomplete(postalAddress);
--- assets/JavaScript/functions.js
+++ assets/JavaScript/functions.js
@@ -1,52 +1,79 @@
-function animateSlides (){
-    const course = document.querySelector('.home');
+function animateSlides() {
+  const course = document.querySelector(".home");
 
-    let slideIndex = 0;
-    let nbSlides = 4;
+  let slideIndex = 0;
+  let nbSlides = 4;
 
-    function moveSlide() {
-        slideIndex++;
+  function moveSlide() {
+    slideIndex++;
 
-        if(slideIndex >= nbSlides) {
-            slideIndex = 0;
-        }
-
-        course.firstElementChild.style.marginLeft = `calc(-${100 * (slideIndex)}% - ${30 * (slideIndex)}px)`;
+    if (slideIndex >= nbSlides) {
+      slideIndex = 0;
     }
 
-    setInterval(() => {
-        moveSlide();
-    }, 3000);
+    course.firstElementChild.style.marginLeft = `calc(-${100 * slideIndex}% - ${
+      30 * slideIndex
+    }px)`;
+  }
+
+  setInterval(() => {
+    moveSlide();
+  }, 3000);
 }
 
 // Set the date we're counting down to
 var countDownDate = new Date("Apr 1, 2023 14:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // Get today's date and time
   var now = new Date().getTime();
-    
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   // Output the result in an element with id="demo"
   document.getElementById("days").innerHTML = days + " /";
   document.getElementById("hours").innerHTML = hours + " / ";
   document.getElementById("minutes").innerHTML = minutes + " / ";
   document.getElementById("seconds").innerHTML = seconds + "";
-  // If the count down is over, write some text 
+  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-animateSlides()
+  var firstName = document.querySelector("#firstName").value;
+  var lastName = document.querySelector("#lastName").value;
+  var email = document.querySelector("#email").value;
+  var mobilePhone = document.querySelector("#mobilePhone").value;
+  var postalAddress = document.querySelector("#postalAddress").value;
+  var dob = document.querySelector("#dob").value;
+
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !mobilePhone ||
+    !postalAddress ||
+    !dob
+  ) {
+    alert("All fields are required!");
+    return;
+  }
+
+  alert("Form submitted successfully!");
+});
+
+
+
+animateSlides();
